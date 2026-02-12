1e45a651b3f6877373dc4f626e3b489060d79886
Aakashnelgikar <143541494+Aakashnelgikar@users.noreply.github.com>
2026-02-12 11:02:28 +0530

    Add JPA annotations and properties to Prop class

--- src/main/java/org/ust/prop/model/Prop.java
+++ src/main/java/org/ust/prop/model/Prop.java
@@ -1,4 +1,17 @@
 package org.ust.prop.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
 public class Prop {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String address;
+    private String description;
 }
