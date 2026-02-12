394adb1970a3d312077916b4f8518964acadab2b
Aakashnelgikar <143541494+Aakashnelgikar@users.noreply.github.com>
2026-02-12 11:03:24 +0530

    Implement PropService with CRUD operations

--- src/main/java/org/ust/prop/service/PropService.java
+++ src/main/java/org/ust/prop/service/PropService.java
@@ -1,4 +1,51 @@
 package org.ust.prop.service;
 
+import org.aspectj.apache.bcel.classfile.annotation.RuntimeTypeAnnos;
+import org.springframework.boot.context.config.ConfigDataResourceNotFoundException;
+import org.springframework.stereotype.Service;
+import org.ust.prop.model.Prop;
+import org.ust.prop.repository.PropRepository;
+
+import java.util.List;
+
+@Service
 public class PropService {
-}
+    public PropRepository propRepository;
+
+    public PropService(PropRepository propRepository){
+        this.propRepository = propRepository;
+    }
+
+    public Prop addProp(Prop prop){
+        return propRepository.save(prop);
+    }
+
+    public List<Prop> getAllProp(){
+        return propRepository.findAll();
+    }
+
+    public Prop getPropById(Long id){
+        return propRepository.findById(id).orElse(null);
+    }
+
+    public Prop updateProp(long id, Prop prop) {
+        Prop existing = getPropById(id);
+        if (prop.getAddress() != null){
+            existing.setAddress(prop.getAddress());
+        }
+        if (prop.getDescription() != null){
+            existing.setDescription(prop.getDescription());
+        }
+        return propRepository.save(existing);
+    }
+
+    public void deleteProp(Long id){
+        propRepository.deleteById(id);
+    }
+
+
+
+    }
+
+
+
