2ea5eba5840ec8b794e1bfcb780612a6bd222d18
Aakashnelgikar <143541494+Aakashnelgikar@users.noreply.github.com>
2026-02-12 11:02:57 +0530

    Extend PropRepository to use JpaRepository

--- src/main/java/org/ust/prop/repository/PropRepository.java
+++ src/main/java/org/ust/prop/repository/PropRepository.java
@@ -1,4 +1,10 @@
 package org.ust.prop.repository;
 
-public interface PropRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.ust.prop.model.Prop;
+@Repository
+
+public interface PropRepository extends JpaRepository<Prop, Long> {
+
 }
